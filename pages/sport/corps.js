b0aef2a0115efdfc8df3eb6e47295469fd387d9a
draizKUN <benoit.antunes@ecoles-wis.net>
2022-06-21 23:36:51 +0200

    np

--- pages/sport/corps.js
+++ pages/sport/corps.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 function corps() {
   return (
-    <div>corps</div>
+    <div className="mb-20">
+      <Header />
+
+      <div className="p-5">
+        <h1 className="text-2xl">Entrainement sur tout le corps</h1>
+      </div>
+
+      <div className="pl-5">
+        <h2 className="text-lg pb-2">Pectoraux</h2>
+        <iframe className="w-[90%]" src="https://www.youtube.com/embed/XQSvE-8Kgp4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+
+      <div className="pl-5 pt-5">
+        <h2 className="text-lg pb-2">Bras</h2>
+        <iframe className="w-[90%]" src="https://www.youtube.com/embed/PHTe-6kQAW0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+
+      <div className="pl-5 pt-5">
+        <h2 className="text-lg pb-2">Jambes</h2>
+        <iframe className="w-[90%]" src="https://www.youtube.com/embed/IcszGajGs0E" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+
+      <div className="pl-5 pt-5">
+        <h2 className="text-lg pb-2">Épaules</h2>
+        <iframe className="w-[90%]" src="https://www.youtube.com/embed/1noV0QbxgIU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+
+      <div className="pl-5 pt-5">
+        <h2 className="text-lg pb-2">Dos</h2>
+        <iframe className="w-[90%]" src="https://www.youtube.com/embed/dH0Ef2hZ_p4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+
+      <div className="pl-5 pt-5">
+        <h2 className="text-lg pb-2">Abdominaux</h2>
+        <iframe className="w-[90%]" src="https://www.youtube.com/embed/PtNyOHpMkJU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+
+    </div>
   )
 }
 
