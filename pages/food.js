94ddd40cad9e1632147820f95511ee73bcf41d86
draizKUN <benoit.antunes@ecoles-wis.net>
2022-06-21 22:06:12 +0200

    sp

---
+++ pages/food.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function food() {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default food
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { IoFastFoodOutline } from "react-icons/io5";
 import Link from 'next/link';
 
 export default function Home() {
@@ -31,6 +32,54 @@ export default function Home() {
         </div>
       </Link>
 
+      {/* Food */}
+      <div className="mt-5 ml-5">
+        <h2 className="text-xl">La Nutrition</h2>
+      </div>
+
+      <div className="mt-5 space-y-5">
+        <Link href="/food">
+          <div className="h-[60px] w-[90%] bg-blue-500 mx-auto rounded-lg flex justify-between items-center px-4">
+            <div className="bg-white h-[50%] w-[10%] rounded-lg flex justify-center items-center">
+              <IoFastFoodOutline color="blue" fontSize={20} />
+            </div>
+            <h3 className="text-white text-xl ml-1">Riz</h3>
+            <span className="text-white text-sm font-light">7g de protéines - 330g...</span>
+            <div>
+              <BsFillArrowRightCircleFill fontSize={20} color="white" />
+            </div>
+          </div>
+        </Link>
+        <Link href="/food">
+          <div className="h-[60px] w-[90%] bg-blue-500 mx-auto rounded-lg flex justify-between items-center px-4">
+            <div className="bg-white h-[50%] w-[10%] rounded-lg flex justify-center items-center">
+              <IoFastFoodOutline color="blue" fontSize={20} />
+            </div>
+            <h3 className="text-white text-xl ml-1">Poulet</h3>
+            <span className="text-white text-sm font-light">60g de protéines...</span>
+            <div>
+              <BsFillArrowRightCircleFill fontSize={20} color="white" />
+            </div>
+          </div>
+        </Link>
+        <Link href="/food">
+          <div className="h-[60px] w-[90%] bg-blue-500 mx-auto rounded-lg flex justify-between items-center px-4">
+            <div className="bg-white h-[50%] w-[10%] rounded-lg flex justify-center items-center">
+              <IoFastFoodOutline color="blue" fontSize={20} />
+            </div>
+            <h3 className="text-white text-xl ml-1">Poisson</h3>
+            <span className="text-white text-sm font-light">71g de protéines...</span>
+            <div>
+              <BsFillArrowRightCircleFill fontSize={20} color="white" />
+            </div>
+          </div>
+        </Link>
+        <Link href="/food">
+          <div className="flex justify-center">
+            <button className="border border-blue-500 border-2 text-blue-500 px-4 py-2 rounded-lg text-sm hover:bg-blue-500 hover:text-white">En Savoir Plus</button>
+          </div>
+        </Link>
+      </div>
 
     </div>
   )
