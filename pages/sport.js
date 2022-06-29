1a39f16ec16ef92d0f7aabfda56439777e949a15
draizKUN <benoit.antunes@ecoles-wis.net>
2022-06-29 14:18:49 +0200

    casca

--- pages/sport.js
+++ pages/sport.js
@@ -15,7 +15,7 @@ function sport() {
       </div>
 
       <Link href="/sport/corps">
-        <div className="relative h-[120px] w-[93%] bg-blue-900 mx-auto rounded-lg pt-3 pl-4">
+        <div className="relative h-[120px] w-[97%] bg-blue-900 mx-auto rounded-lg pt-3 pl-4">
           <div className="flex space-x-2 items-center">
             <GiMuscleUp fontSize={20} color="white" />
             <h2 className="text-lg text-white">Entrainement - Corps Entier</h2>
@@ -28,53 +28,53 @@ function sport() {
         </div>
       </Link>
 
-      <div className="mt-5 grid grid-cols-2 gap-y-5 place-items-center justify-self-center">
+      <div className="mt-5 grid grid-cols-3 gap-y-5 place-items-center justify-self-center">
         <Link href="/sport/pectoraux">
-          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[90%] h-[90px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <Image src="/pectoraux.png" height="60" width="60"/>
+              <Image src="/pectoraux.png" height="40" width="40"/>
             </div>
-            <h2 className="text-white text-xl text-center">Pectoraux</h2>
+            <h2 className="text-white text-md text-center">Pectoraux</h2>
           </div>
         </Link>
         <Link href="/sport/bras">
-          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[90%] h-[90px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <Image src="/bras.png" height="50" width="50"/>
+              <Image src="/bras.png" height="35" width="35"/>
             </div>
-            <h2 className="text-white text-xl text-center">Bras</h2>
+            <h2 className="text-white text-md text-center">Bras</h2>
           </div>
         </Link>
         <Link href="/sport/abdominaux">
-          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[90%] h-[90px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <Image src="/abdominal.png" height="50" width="50"/>
+              <Image src="/abdominal.png" height="35" width="35"/>
             </div>
-            <h2 className="text-white text-xl text-center">Abdominaux</h2>
+            <h2 className="text-white text-md text-center">Abdominaux</h2>
           </div>
         </Link>
         <Link href="/sport/jambes">
-          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[90%] h-[90px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <Image src="/jambe.png" height="50" width="50"/> 
+              <Image src="/jambe.png" height="35" width="35"/> 
             </div>
-            <h2 className="text-white text-xl text-center">Jambes</h2>
+            <h2 className="text-white text-md text-center">Jambes</h2>
           </div>
         </Link>
         <Link href="/sport/epaules">
-          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[90%] h-[90px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <Image src="/epaule.png" height="50" width="50"/>
+              <Image src="/epaule.png" height="35" width="35"/>
             </div>
-            <h2 className="text-white text-xl text-center">Épaules</h2>
+            <h2 className="text-white text-md text-center">Épaules</h2>
           </div>
         </Link>
         <Link href="/sport/dos">
-          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[90%] h-[90px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <Image src="/dos-humain.png" height="50" width="50"/>
+              <Image src="/dos-humain.png" height="35" width="35"/>
             </div>
-            <h2 className="text-white text-xl text-center">Dos</h2>
+            <h2 className="text-white text-md text-center">Dos</h2>
           </div>
         </Link>
       </div>
