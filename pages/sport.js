6b399383430c394c4f201d48b10c2baa563abba7
draizKUN <benoit.antunes@ecoles-wis.net>
2022-06-29 13:22:13 +0200

    guts

--- pages/sport.js
+++ pages/sport.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import Image from "next/image"
 import React from 'react'
 import Header from '../components/Header'
-import { BsFillArrowRightCircleFill } from "react-icons/bs";
-import { GiMuscleUp } from "react-icons/gi";
+import { BsFillArrowRightCircleFill } from "react-icons/bs"
+import { GiMuscleUp } from "react-icons/gi"
 
 function sport() {
   return (
@@ -14,8 +15,11 @@ function sport() {
       </div>
 
       <Link href="/sport/corps">
-        <div className="relative h-[120px] w-[93%] bg-blue-400 mx-auto rounded-lg pt-3 pl-4">
-          <h2 className="text-lg text-white">Entrainement - Corps Entier</h2>
+        <div className="relative h-[120px] w-[93%] bg-blue-900 mx-auto rounded-lg pt-3 pl-4">
+          <div className="flex space-x-2 items-center">
+            <GiMuscleUp fontSize={20} color="white" />
+            <h2 className="text-lg text-white">Entrainement - Corps Entier</h2>
+          </div>
           <span className="text-white font-light">Pectoraux - Abdominaux - Bras - Dos...</span>
 
           <div className="absolute bottom-3 right-3">
@@ -24,51 +28,51 @@ function sport() {
         </div>
       </Link>
 
-      <div className="w-full mt-5 grid grid-cols-2 gap-y-5 place-items-center justify-self-center">
+      <div className="mt-5 grid grid-cols-2 gap-y-5 place-items-center justify-self-center">
         <Link href="/sport/pectoraux">
-          <div className="bg-blue-900 w-[88%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <GiMuscleUp fontSize={40} color="white" />
+              <Image src="/pectoraux.png" height="60" width="60"/>
             </div>
             <h2 className="text-white text-xl text-center">Pectoraux</h2>
           </div>
         </Link>
         <Link href="/sport/bras">
-          <div className="bg-blue-900 w-[88%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <GiMuscleUp fontSize={40} color="white" />
+              <Image src="/bras.png" height="50" width="50"/>
             </div>
             <h2 className="text-white text-xl text-center">Bras</h2>
           </div>
         </Link>
         <Link href="/sport/abdominaux">
-          <div className="bg-blue-900 w-[88%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <GiMuscleUp fontSize={40} color="white" />
+              <Image src="/abdominal.png" height="50" width="50"/>
             </div>
             <h2 className="text-white text-xl text-center">Abdominaux</h2>
           </div>
         </Link>
         <Link href="/sport/jambes">
-          <div className="bg-blue-900 w-[88%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <GiMuscleUp fontSize={40} color="white" />
+              <Image src="/jambe.png" height="50" width="50"/> 
             </div>
             <h2 className="text-white text-xl text-center">Jambes</h2>
           </div>
         </Link>
         <Link href="/sport/epaules">
-          <div className="bg-blue-900 w-[88%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <GiMuscleUp fontSize={40} color="white" />
+              <Image src="/epaule.png" height="50" width="50"/>
             </div>
             <h2 className="text-white text-xl text-center">Épaules</h2>
           </div>
         </Link>
         <Link href="/sport/dos">
-          <div className="bg-blue-900 w-[88%] h-[130px] rounded-lg flex flex-col justify-center items-center">
+          <div className="bg-blue-400 w-[80%] h-[130px] rounded-lg flex flex-col justify-center items-center">
             <div className="flex justify-center items-center mb-3">
-              <GiMuscleUp fontSize={40} color="white" />
+              <Image src="/dos-humain.png" height="50" width="50"/>
             </div>
             <h2 className="text-white text-xl text-center">Dos</h2>
           </div>
